cce43b24a62ef01e52b6217c0a3aa0f0cc5d5fd3
Patricia Gracia Gutierrez <pgg.uxui@gmail.com>
2025-02-04 18:21:09 +0000

    Solved

---
+++ vite-project/src/components/Hero.jsx
@@ -0,0 +1,21 @@
+export default function Hero() {
+    return (
+      <section id="hero-section">
+        <h1>IMAGINE A PLACE...</h1>
+        <p>...where you can belong to a school club, 
+            a gaming group, or a worldwide art community.
+            Where just you and a handful of friends can spend
+            time together. A place that makes it easy to talk
+            every day and hang out more often.
+        </p>
+        <button id="download-button">
+            Download for Mac
+        </button>
+        <button id="open-button">
+            Open Discord in your browser
+        </button>
+        
+      </section>
+    );
+  }
+  
---
+++ vite-project/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+export default function Navbar() {
+    return (
+      <nav>
+        <img
+          id="logo"
+          src= "../assets/discord-logo-white"
+          alt="logo discord"
+        />
+        <img
+          id="nav-img"
+          src= "../assets/menu-icon.png"
+          alt="icon hamburger"
+        />
+        
+      </nav>
+    );
+  }
+  
